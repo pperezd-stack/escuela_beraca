11a1d3ced97eadec7e356e7a126d182c62994f5d
pperezd-stack <pperezd@unicartagena.edu.co>
2026-07-27 10:34:45 -0500

    Add method to find module by professor ID
    Added a new method to find a module by professor ID.

--- sql Backend Beraca/plataforma-beraca-api-master/src/main/java/com/beraca/Repository/ModuloRepository.java
+++ sql Backend Beraca/plataforma-beraca-api-master/src/main/java/com/beraca/Repository/ModuloRepository.java
@@ -2,9 +2,14 @@ package com.beraca.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.beraca.model.Modulo;
 
+import java.util.Optional;
+
 @Repository
-public interface ModuloRepository extends JpaRepository<Modulo,Long>{
+public interface ModuloRepository extends JpaRepository<Modulo, Long> {
+
+    // 🟢 Nuevo método: busca el módulo que tiene asignado a un profesor específico
+    Optional<Modulo> findByProfesorId(Long profesorId);
+
 }
