6b56a8762973057f640d97b269e4208fdd4497a4
pperezd-stack <pperezd@unicartagena.edu.co>
2026-07-31 12:31:50 -0500

    Add method to fetch modules by student ID
    Added a new query method to find modules by student ID.

--- sql Backend Beraca/plataforma-beraca-api-master/src/main/java/com/beraca/Repository/ModuloRepository.java
+++ sql Backend Beraca/plataforma-beraca-api-master/src/main/java/com/beraca/Repository/ModuloRepository.java
@@ -1,15 +1,18 @@
 package com.beraca.Repository;
+package com.beraca.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.beraca.model.Modulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface ModuloRepository extends JpaRepository<Modulo, Long> {
 
-    // 🟢 Nuevo método: busca el módulo que tiene asignado a un profesor específico
-    Optional<Modulo> findByProfesorId(Long profesorId);
-
+    // Añade esta consulta para que reconozca el método que usamos en el login:
+    @Query(value = "SELECT m.* FROM modulos m INNER JOIN estudiante_modulo em ON m.id = em.modulo_id WHERE em.estudiante_id = :estudianteId", nativeQuery = true)
+    List<Modulo> findModulosByEstudianteId(@Param("estudianteId") Long estudianteId);
 }
