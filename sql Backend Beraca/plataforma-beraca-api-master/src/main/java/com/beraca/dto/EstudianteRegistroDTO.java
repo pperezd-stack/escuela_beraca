00f0bcc729fd112a5440c68e8fcf294af9613842
pperezd-stack <pperezd@unicartagena.edu.co>
2026-07-31 11:39:57 -0500

    Add EstudianteRegistroDTO class for student registration

---
+++ sql Backend Beraca/plataforma-beraca-api-master/src/main/java/com/beraca/dto/EstudianteRegistroDTO.java
@@ -0,0 +1,42 @@
+package com.beraca.dto;
+
+public class EstudianteRegistroDTO {
+
+    private String nombre;
+    private String password;
+    private String rol;
+    private Long moduloId;
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Long getModuloId() {
+        return moduloId;
+    }
+
+    public void setModuloId(Long moduloId) {
+        this.moduloId = moduloId;
+    }
+}
